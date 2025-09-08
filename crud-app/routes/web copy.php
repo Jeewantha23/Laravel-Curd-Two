bde04a187263dc2466454e053c9f53cab3f923ea
Jeewantha priyasad <rathnayakamaxxx@gmail.com>
2025-09-08 14:11:20 +0530

    Updated Content

---
+++ crud-app/routes/web copy.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\TeacherController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/teachers',[TeacherController::class,'index' ])->name('Teachers.index');
+// Route::get('/teachers/create',[TeacherController::class,'create' ])->name('Teachers.create');
+// Route::post('/teachers',[TeacherController::class,'store' ])->name('Teachers.store');
+// Route::get('/teachers/{id}',[TeacherController::class,'show' ])->name('Teachers.show');
+// Route::get('/teachers/{id}/edit',[TeacherController::class,'edit' ])->name('Teachers.edit');
+// Route::put('/teachers/{id}',[TeacherController::class,'update' ])->name('Teachers.update');
+// Route::delete('/teachers/{id}',[TeacherController::class,'destroy' ])->name('Teachers.destroy');
